b3fa9eb0780002dd1cccebe0a3eb515667bc5b08
LegendaryPloxerd <mimojih@gmail.com>
2025-05-23 02:31:07 +0200

    wrapper startoff

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './index.css'
+import { useEffect } from 'react'
+
+function App() {
+  useEffect(() => {
+  })
+  return (
+    <div className="background h-screen bg-gray-900 flex justify-center items-center">
+      <div className="wrapper bg-[#414141] w-[35rem] h-[35rem] rounded-3xl">
+        <div className="">
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App

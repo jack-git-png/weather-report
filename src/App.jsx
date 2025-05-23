987637383bbc015f917d6ffce5c04df5be525850
LegendaryPloxerd <mimojih@gmail.com>
2025-05-23 02:58:55 +0200

    changes, additions.

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,11 @@ function App() {
   })
   return (
     <div className="background h-screen bg-gray-900 flex justify-center items-center">
-      <div className="wrapper bg-[#414141] w-[35rem] h-[35rem] rounded-3xl">
-        <div className="">
-
-        </div>
+      <div className="wrapper bg-[#414141] w-[35rem] h-[35rem] rounded-3xl items-center content-center text-center">
+        <form method="get">
+          <label className="text-2xl text-white font-bold color" htmlFor="counntry-name">Search the weather for many countries!</label><br />
+          <input type="search" className="border-2 border-white text-2xl rounded-2xl"/>
+        </form>
       </div>
     </div>
   )
